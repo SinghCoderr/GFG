977438abcb3c9e14b8a7d219b22ffd34e19b20d4
SinghCoderr <aappuussii5087@gmail.com>
2025-04-19 12:46:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Move all negative elements to end/move-all-negative-elements-to-end.java
@@ -0,0 +1,66 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = Integer.parseInt(sc.nextLine());
+
+        while (T > 0) {
+            String[] input = sc.nextLine().trim().split(" ");
+            int[] a = new int[input.length];
+            for (int i = 0; i < input.length; i++) {
+                a[i] = Integer.parseInt(input[i]);
+            }
+            Solution ob = new Solution();
+            ob.segregateElements(a);
+            for (int num : a) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+            T--;
+        
+System.out.println("~");
+}
+        sc.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public void segregateElements(int[] arr) {
+        // Your code goes here
+    int n = arr.length;
+        int[] temp = new int[n];
+        int index = 0;
+
+        // Step 1: Copy positive numbers in order
+        for (int i = 0; i < n; i++) {
+            if (arr[i] >= 0) {
+                temp[index++] = arr[i];
+            }
+        }
+
+        // Step 2: Copy negative numbers in order
+        for (int i = 0; i < n; i++) {
+            if (arr[i] < 0) {
+                temp[index++] = arr[i];
+            }
+        }
+
+        // Step 3: Copy everything back to original array
+        for (int i = 0; i < n; i++) {
+            arr[i] = temp[i];
+        }
+    
+    
+}
+}
