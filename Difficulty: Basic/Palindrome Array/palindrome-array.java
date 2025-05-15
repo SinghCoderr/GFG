4861db0c74ffb5cb110e13f0c5e35a3091a527a8
SinghCoderr <aappuussii5087@gmail.com>
2025-05-16 01:04:09 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Palindrome Array/palindrome-array.java
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine().trim());
+
+        while (t-- > 0) {
+            String line = read.readLine().trim();
+            String[] numsStr = line.split(" ");
+            int[] nums = new int[numsStr.length];
+            for (int i = 0; i < numsStr.length; i++) {
+                nums[i] = Integer.parseInt(numsStr[i]);
+            }
+            Solution obj = new Solution();
+            boolean res = obj.isPerfect(nums);
+            if (res)
+                System.out.println("true");
+
+            else
+                System.out.println("false");
+        
+System.out.println("~");
+}
+    }
+}
+
+// } Driver Code Ends
+
+
+
+class Solution {
+    public static boolean isPerfect(int[] arr) {
+        // code here
+        int n=arr.length;
+        int i=0;
+        int j=n-1;
+        while(i<j){
+            if(arr[i]!=arr[j]){
+        return false;
+            }
+i++;
+j--;
+        }
+        return true;
+    }
+}
