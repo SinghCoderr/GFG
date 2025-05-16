9bcd04ed3aea5e05dcfdb204b537357768fdf4d5
SinghCoderr <aappuussii5087@gmail.com>
2025-05-16 11:04:15 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/First Digit of a Number/first-digit-of-a-number.java
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+
+class GFG{
+    public static void main(String arg[]) throws IOException{
+        BufferedReader read = new BufferedReader( new InputStreamReader(System.in));
+        
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0){
+            int n = Integer.parseInt(read.readLine());
+            
+            int answer = firstDigit(n);
+            System.out.println(answer);
+        
+System.out.println("~");
+}
+    }
+    
+    
+// } Driver Code Ends
+
+// User function Template for Java
+
+// Complete the function
+public static int firstDigit(int n) {
+    // code here
+     while (n >= 10) {
+        n /= 10;
+    }
+    return n;
+}
+
+
+//{ Driver Code Starts.
+}
+// } Driver Code Ends
