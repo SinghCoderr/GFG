c669776b567a69e779d54bf7f3d6a605650530c7
SinghCoderr <aappuussii5087@gmail.com>
2025-07-06 11:08:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Remove Duplicates Sorted Array/remove-duplicates-sorted-array.java
+++ Difficulty: Easy/Remove Duplicates Sorted Array/remove-duplicates-sorted-array.java
@@ -1,42 +1,16 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-public class Main {
-    public static void main(String args[]) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int tc = Integer.parseInt(br.readLine());
-        while (tc-- > 0) {
-            String str[] = br.readLine().split(" ");
-            int arr[] = new int[str.length];
-            for (int i = 0; i < str.length; i++) arr[i] = Integer.parseInt(str[i]);
-            Solution obj = new Solution();
-            int len = obj.removeDuplicates(arr);
-            for (int i = 0; i < len; i++) {
-                System.out.print(arr[i] + " ");
-            }
-            System.out.println();
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
-
-
-// User function Template for Java
-
 class Solution {
     // Function to remove duplicates from the given array
     public int removeDuplicates(int[] arr) {
         // Code Here
-        int i=0;
-        int n=arr.length;
-        for(int j=1;j<n;j++){
-            if(arr[i]!=arr[j]){
-                i++;
-                arr[i]=arr[j];
+          if (arr.length == 0) return 0;
+
+        int index = 1;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] != arr[i - 1]) {
+                arr[index] = arr[i];
+                index++;
             }
         }
-        return i+1;
+        return index;
     }
 }
