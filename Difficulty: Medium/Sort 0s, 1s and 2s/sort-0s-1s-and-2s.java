c876cfa12947bc010d72dee44ddef4358163222f
SinghCoderr <aappuussii5087@gmail.com>
2026-04-11 09:30:36 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.java
+++ Difficulty: Medium/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.java
@@ -1,58 +1,27 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int t = Integer.parseInt(br.readLine());
-
-        while (t-- > 0) {
-            String input = br.readLine();
-            String[] inputArray = input.split("\\s+");
-            int a[] = new int[inputArray.length];
-
-            for (int i = 0; i < a.length; i++) a[i] = Integer.parseInt(inputArray[i]);
-
-            Solution ob = new Solution();
-            ob.sort012(a);
-
-            for (int num : a) {
-                System.out.print(num + " ");
-            }
-            System.out.println();
-            System.out.println("~");
-        }
-    }
-}
-
-
-// } Driver Code Ends
 class Solution {
-    // Function to sort an array of 0s, 1s, and 2s
     public void sort012(int[] arr) {
+        // code here
         int n=arr.length;
-        int count0=0,count1=0,count2=0;
-        for(int i=0;i<n;i++){
-            if(arr[i]==0)
-            count0++;
-            else if(arr[i]==1)
-            count1++;
-            else count2++;
-        }
-        int index=0;
-        for(int i=0;i<count0;i++){
-            arr[index++]=0;
-        }
-        for(int i=0;i<count1;i++){
-            arr[index++]=1;
-        }
-        for(int i=0;i<count2;i++){
-            arr[index++]=2;
+        int l=0,r=n-1,j=0;
+        while(j<=r){
+            if(arr[j]==1){
+            j++;
+            }
+            else if(arr[j]==2){
+                // swap(arr[j],arr[r]);
+                int t=arr[j];
+                arr[j]=arr[r];
+                arr[r]=t;
+                r--;
+            }
+            else{
+                // swap(arr[j],arr[l]);
+                int temp=arr[j];
+                arr[j]=arr[l];
+                arr[l]=temp;
+                l++;
+                j++;
+            }
         }
     }
 }
-
-//{ Driver Code Starts.
-// } Driver Code Ends
